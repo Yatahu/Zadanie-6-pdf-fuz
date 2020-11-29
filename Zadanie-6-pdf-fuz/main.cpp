ddc88b301cff9012b46a563783e64a6a7b7ebde2
Wojtek Szyjka <wojtus0708@gmail.com>
2020-11-29 23:46:27 +0100

    Zadanie-6-pdf-fuz

--- Zadanie-6-pdf-fuz/main.cpp
+++ Zadanie-6-pdf-fuz/main.cpp
@@ -5,10 +5,34 @@
 //  Created by Wojciech Szyjka on 29/11/2020.
 //
 
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+ 
+int NWD(int a, int b)
+{
+    int pom;
+    
+    while(b!=0)
+    {
+        pom = b;
+        b = a%b;
+        a = pom;
+    }
+    
+    return a;
+}
+ 
+int main()
+{
+    int a, b;
+ 
+    cout << "Podaj dwie liczby:  ";
+    cin >> a;
+    cout << endl << "a teraz drugą : " ;
+    cin >> b ;
+ 
+    cout << "NWD(" << a << "," <<b<< ") = "<< NWD(a,b) << endl;
+ 
     return 0;
 }
